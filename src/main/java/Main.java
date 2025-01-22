8cd04c80ae64762ecedfe2eb1b26bec466fbf98c
Valdemar <s224289@dtu.dk>
2025-01-22 17:00:47 +0100

    added the first steps, has a gui and can draw on gui, first shortcut also added ctrl + z

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import screens.paintScreen;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        new paintScreen();
     }
 }
